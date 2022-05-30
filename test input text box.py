0d83e0f2f0349db05cdb1f81c8a076422f1d3d65
chacharasek <95644847+chacharasek@users.noreply.github.com>
2022-05-30 16:37:49 +0200

    Delete test input text box.py

--- test input text box.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-with open(r"klikacka/leaderboard.ini", 'a') as config:
-    config.write(username + ":" + "0" +"\n")
-with open(r"klikacka/leaderboard.ini", 'r+') as config:
-    config.write("maxScore" + ":" + "583\n")
-    config.seek(0)
